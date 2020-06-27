04fd48649c8b514a0a0fff9700eacd25a0795b92
Ravuthz <ravuthz@gmail.com>
2020-06-27 17:19:10 +0700

    Add Config and ENV

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from 'nestjs-config';
+import * as path from 'path';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const NODE_ENV = process.env.NODE_ENV;
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.load(path.resolve(__dirname, 'config', '**/!(*.d).{ts,js}'), {
+      path: path.resolve(process.cwd(), !NODE_ENV ? '.env' : `.env.${NODE_ENV}`),
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from 'nestjs-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  const config = app.get(ConfigService);
+  const port = config.get('app.port');
+
+  await app.listen(port).then(() => {
+    Logger.log(`Server running on http://localhost:${port}`, 'Bootstrap');
+  });
 }
+
 bootstrap();
